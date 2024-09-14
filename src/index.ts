df11a3aca8bb983bd1be287eee8cf31234b668a3
Tom McLaughlin <tom@serverlessops.io>
2024-09-14 14:00:39 -0400

    Add some vscode launch and debug setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import path from 'path'
+import { compilerOptions } from '../tsconfig.json'
 
 import { createWorkspace } from './createWorkspace'
 
 const AWS_CFN_DOC_ROOT = 'https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/'
 
-const APP_ROOT = path.resolve(__dirname, '../..')
+// Resolve APP_ROOT based on whether TS or compiled JS.
+const APP_ROOT = path.resolve(
+    __dirname,
+    path.basename(path.dirname(__dirname)) == compilerOptions.outDir ? '../..' : '..'
+)
 const DOC_BUILD_ROOT = path.join(APP_ROOT, 'docbuild')
 const DOCSET_DIR = path.join(DOC_BUILD_ROOT, 'aws-cloudformation.docset')
 
