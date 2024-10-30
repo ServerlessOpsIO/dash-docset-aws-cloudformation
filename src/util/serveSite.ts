72a8d085d9799615295ce0adc409cb1dcb2c8d77
Tom McLaughlin <tom@serverlessops.io>
2024-10-30 14:41:14 -0700

    Switch from simple nodejs http server to express for local serving
    express is more resilient to failures

--- src/util/serveSite.ts
+++ src/util/serveSite.ts
@@ -1,51 +1,19 @@
-import * as http from 'http'
-import * as fs from 'fs'
-import * as path from 'path'
+import express from 'express'
+import path from 'path'
 
-const port = 3000
+const app = express()
+const port = process.env.PORT || 3000
 
-export const APP_ROOT = path.resolve(
-    __dirname,
-    '../../..'
-)
-export const SITE_DIR = path.join(APP_ROOT, 'site')
+// Define the directory to serve static files from
+const staticDir = path.resolve(path.join(__dirname, '../../..', 'site'))
 
-const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-    const filePath = path.join(SITE_DIR, req.url || '')
-    const extname = String(path.extname(filePath)).toLowerCase()
-    const mimeTypes: { [key: string]: string } = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.json': 'application/json',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.gif': 'image/gif',
-        '.svg': 'application/image/svg+xml'
-    }
+// Use express.static middleware to serve static files
+app.use(express.static(staticDir))
 
-    const contentType = mimeTypes[extname] || 'application/octet-stream'
-
-    fs.readFile(filePath, (error, content) => {
-        if (error) {
-            if (error.code === 'ENOENT') {
-                res.writeHead(404, { 'Content-Type': 'text/html' })
-                res.end('<h1>404 Not Found</h1>', 'utf-8')
-            } else {
-                res.writeHead(500)
-                res.end(`Server Error: ${error.code}`)
-            }
-        } else {
-            res.writeHead(200, { 'Content-Type': contentType })
-            res.end(content, 'utf-8')
-        }
-    })
-})
-
-
-
+// Start the server if this module is executed as the main module
 if (require.main === module) {
-    server.listen(port, () => {
+    app.listen(port, () => {
+        console.log(`Serving files at ${staticDir}`)
         console.log(`Server running at http://localhost:${port}/`)
     })
 }
