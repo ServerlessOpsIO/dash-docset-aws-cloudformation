3658c8880b9678f58f198a21c3ba2e9026a10bec
Tom McLaughlin <tom@serverlessops.io>
2024-11-03 21:24:55 -0500

    properly highlight code blocks
    We need to handle differences between true code blocks and code blocks with links to page anchors as well as values representing data types

--- src/highlightCode.ts
+++ src/highlightCode.ts
@@ -1,6 +1,7 @@
 import hljs from 'highlight.js'
 import * as cheerio from 'cheerio'
 
+
 /*
  * Highlight code blocks in page
  *
@@ -16,6 +17,31 @@ export async function highlightCode($page: cheerio.CheerioAPI): Promise<void> {
             return
         }
 
+        // Inserting the hljs class gets us the AWS CSS formatting we want.
         $page(e).addClass('hljs')
+
+        let highlightedCode = ''
+        let lines: string[] = []
+        if (e.attribs.class.startsWith('json')) {
+            lines = e.children.map((child) => {
+                if (child.type != 'text') {
+                    return $page.html(child)
+                }
+
+                const text = $page(child).text()
+                return hljs.highlight(text, { language: 'json' }).value
+            })
+        } else if (e.attribs.class.startsWith('yaml')) {
+            lines = e.children.map((child) => {
+                if (child.type != 'text') {
+                    return $page.html(child) as string
+                }
+
+                const text = $page(child).text()
+                return hljs.highlight(text, { language: 'yaml' }).value
+            })
+        }
+
+        $page(e).html(lines.join(''))
     })
 }
